10189f8632a0e431093699c8f5624e200024bd6c
denisa <dmutapcic79@hotmail.com>
2024-09-09 23:12:07 -0400

    Ternary practice

--- src/if_statement_practice/switch_statement/Loans.java
+++ src/if_statement_practice/switch_statement/Loans.java
@@ -1,6 +1,15 @@
 package if_statement_practice.switch_statement;
 
 public class Loans {
+
+    public static void main(String[] args) {
+
+        int salary = 40000;
+        int creditScore = 650;
+        String result=(salary>=60000 && creditScore <=650)? "Loan Approved":"Loan Denied";
+
+        System.out.println(result);
+    }
 }
 /*
 Create a class called Loans, Given two variables salary and credit score, use those given info to determine if you can get a loan.
