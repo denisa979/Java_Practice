01fe39a1857bcc76a359f6269cb2daf10dbef357
denisa <dmutapcic79@hotmail.com>
2024-09-09 23:44:47 -0400

    Switch Statement

--- src/if_statement_practice/switch_statement/HTTP.java
+++ src/if_statement_practice/switch_statement/HTTP.java
@@ -1,6 +1,58 @@
 package if_statement_practice.switch_statement;
 
 public class HTTP {
+    public static void main(String[] args) {
+        int statusCode=200;
+        String result = "";
+        
+        switch (statusCode){
+            case 200:
+                result="OK";
+                break;
+            case 201:
+                result = "Created";
+                break;
+            case 202:
+                result="Accepted";
+                break;
+            case 301:
+                result = "Moved permanently";
+                break;
+            case 303:
+                result="See Other";
+                break;
+            case 304:
+                result = "Not Modify"; 
+                break;
+            case 307:
+                result="Temporary Redirect";
+                break;
+            case 400:
+                result = "Bad Request";
+                break;
+            case 401:
+                result="Unauthorized";
+                break;
+            case 403:
+                result=" Forbidden";
+                break;
+            case 404:
+                result="Not Found";
+                break;
+            case 410:
+                result="Gone";
+                break;
+            case 500:
+                result="Internal Server Error";
+                break;
+            case 503:
+                result="Service Unavailable";
+                break;
+            default:
+                result="No Such a Status Code";
+        }
+        System.out.println(result);
+    }
 }
 /*
 HTTP is the protocol that governs communications between web-servers and web clients.
