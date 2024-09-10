ca5452044aec08c5817d7df5143b994bdce1eeaf
denisa <dmutapcic79@hotmail.com>
2024-09-09 23:22:31 -0400

    Ternary practice

--- src/if_statement_practice/switch_statement/NumberToWord.java
+++ src/if_statement_practice/switch_statement/NumberToWord.java
@@ -1,6 +1,14 @@
 package if_statement_practice.switch_statement;
 
 public class NumberToWord {
+
+    public static void main(String[] args) {
+        int number =8;
+        String result;
+        String word=(number ==0)?"Zero":(number==1)? "One":(number==2)?"Two":(number==3)?"Three":(number==4)?"Four":(number==5)?"Five":(number==6)?"Six":(number==7)?"Seven":(number==8)?"Eight":(number==9)?"Nine":"Invalid Number";
+        System.out.println(word);
+
+    }
 }
 
 /*
