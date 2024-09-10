8ddb0e4cc8e6feec7163cc0d43e5daf304971175
denisa <dmutapcic79@hotmail.com>
2024-09-09 22:28:00 -0400

    Switch statement practice

--- src/if_statement_practice/switch_statement/BiggerNum.java
+++ src/if_statement_practice/switch_statement/BiggerNum.java
@@ -1,6 +1,16 @@
 package if_statement_practice.switch_statement;
 
 public class BiggerNum {
+
+    public static void main(String[] args) {
+
+        int n1 =10;
+        int n2 =20;
+        int n3 =10;
+        String result=(n1>n2 && n1>n3)? "n1 is bigger":(n2>n1 && n2>n3)? "n2 is bigger":("n3 is bigger");
+        System.out.println(result);
+
+    }
 }
 /*
 Create a class called BiggerNum, write a program that checks for the bigger of 3 numbers.
--- src/if_statement_practice/switch_statement/Browser.java
+++ src/if_statement_practice/switch_statement/Browser.java
@@ -1,6 +1,36 @@
 package if_statement_practice.switch_statement;
 
+import java.util.Locale;
+
 public class Browser {
+    public static void main(String[] args) {
+
+       String browser="Safari";
+       String result;
+
+       switch (browser.toLowerCase()){
+           case "chrome":
+               result = "Chrome is selected";
+               break;
+           case "firefox":
+               result = "Firefox is selected";
+               break;
+           case "opera":
+               result = "Opera is selected";
+               break;
+           case "safari":
+               result = "Safari is selected";
+               break;
+           case "edge":
+               result = "Edge is selected";
+               break;
+           default:
+               result = "Invalid Browser";
+               break;
+
+       }
+        System.out.println(result);
+    }
 }
 /*
 write a program that can display the selected browser3.1  declare a String variable called browserName3.2  Assume that the valid browsers are: chrome, firefox, opera, safari, edge3.3 if the browser name does not match with the valid browsers' names, out put should be: Invalid Browser
