590e65c3d4735cb0ab1677bc1922ba177f4ccd1a
denisa <dmutapcic79@hotmail.com>
2024-09-12 23:03:20 -0400

    eligible to vote task

--- src/method/EligibleToVote.java
+++ src/method/EligibleToVote.java
@@ -2,6 +2,18 @@ package method;
 
 public class EligibleToVote {
 
+    public static void main(String[] args) {
+        eligibleToVote(18,false);
+    }
+    public static void eligibleToVote(int age, boolean isAmerican){
+
+        if(age <= 18 && isAmerican){
+            System.out.println("Eligible to vote");
+        }else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
 
 }
 /*
