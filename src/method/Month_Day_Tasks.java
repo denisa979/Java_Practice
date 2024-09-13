c3205b259bb01062548271da3b784c6421303e89
denisa <dmutapcic79@hotmail.com>
2024-09-12 23:52:51 -0400

    days in the month

--- src/method/Month_Day_Tasks.java
+++ src/method/Month_Day_Tasks.java
@@ -6,7 +6,19 @@ public class Month_Day_Tasks {
         day(3);
         day(10);
 
+        System.out.println("----------------------------------------------------------");
+
         System.out.println(month(2));
+        System.out.println(month(7));
+
+        System.out.println("----------------------------------------------------------");
+
+        daysInMonth("JuNe");
+        daysInMonth("AuGust");
+        daysInMonth("Ooctober");
+
+        System.out.println("-----------------------------------------------------------");
+
 
     }
     public static void day(int n){
@@ -36,8 +48,6 @@ public class Month_Day_Tasks {
                 break;
         }
     }
-
-
     public static String month( int monthName) {
         switch (monthName) {
             case 1:
@@ -66,10 +76,54 @@ public class Month_Day_Tasks {
                 return "December";
             default:
                 return "Invalid month";
-
         }
     }
 
+    public static String daysInMonth(String month) {
+
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("January has 31 days");
+                break;
+            case "february":
+                System.out.println("February has 28 days");
+                break;
+            case "march":
+                System.out.println("March as 31 days");
+                break;
+            case "april":
+                System.out.println("April has 30 days");
+                break;
+            case "may":
+                System.out.println("May has 31 days");
+                break;
+            case "june":
+                System.out.println("June has 30 days");
+                break;
+            case "july":
+                System.out.println("July has 31 days");
+                break;
+            case "august":
+                System.out.println("August has 31");
+                break;
+            case "september":
+                System.out.println("September has 30 days");
+                break;
+            case "october":
+                System.out.println("October has 31 days");
+                break;
+            case "november":
+                System.out.println("November has 30 days");
+                break;
+            case "december":
+                System.out.println("December has 31 days");
+                break;
+            default:
+                System.out.println("Invalid month");
+                break;
+        }
+        return month;
+    }
 }
 
 /*
