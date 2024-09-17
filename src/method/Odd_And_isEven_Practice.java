2106d097da7c0c6e02a0dac84dd5edea6f2fd434
denisa <dmutapcic79@hotmail.com>
2024-09-16 23:07:27 -0400

    practice

--- src/method/Odd_And_isEven_Practice.java
+++ src/method/Odd_And_isEven_Practice.java
@@ -2,6 +2,20 @@ package method;
 
 public class Odd_And_isEven_Practice {
 
+    public static void main(String[] args) {
+        System.out.println("isEven(23) = " + isEven(23));
+        System.out.println("isOdd(23) = " + isOdd(23));
+    }
+
+    public static boolean isOdd(int num){
+      return num % 2 !=0;
+
+    }
+
+    public static boolean isEven(int n){
+       return n % 2 ==0;
+    }
+
       /*
        Create a method named isOdd, that can return true if a number is an odd number, otherwise returns false
      */
