8e0d0fc944d39c1950d75ede4b90ba4505a1e6c5
denisa <dmutapcic79@hotmail.com>
2024-09-20 00:58:04 -0400

    diamond

---
+++ src/string/Diamond.java
@@ -0,0 +1,17 @@
+package string;
+
+public class Diamond {
+    public static void main(String[] args) {
+        System.out.println("\t    ^");
+        System.out.println("\t   / \\  ");
+        System.out.println("\t  / * \\  ");
+        System.out.println("\t / * * \\ ");
+        System.out.println("\t/ * * * \\");
+        System.out.println("\t--------- ");
+        System.out.println("\t\\ * * * /");
+        System.out.println("\t \\ * * / ");
+        System.out.println("\t  \\ * /  ");
+        System.out.println("\t   \\ /  ");
+        System.out.println("\t    v");
+    }
+}
