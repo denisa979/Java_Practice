fa68044b12bc0a7ab78d31a82073da9ecd5e6393
denisa <dmutapcic79@hotmail.com>
2024-09-20 03:59:37 -0400

    unique character

--- src/branchingStatements_loop/UniqueCharacters.java
+++ src/branchingStatements_loop/UniqueCharacters.java
@@ -1,6 +1,23 @@
 package branchingStatements_loop;
 
 public class UniqueCharacters {
+
+    public static void main(String[] args) {
+       System.out.println( findUnique("AABCCD") );
+    }
+    public static String findUnique(String str){
+        String unique ="";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(str.indexOf(ch)==str.lastIndexOf(ch)){
+                unique+=ch;
+            }
+        }
+        return unique;
+
+    }
+
 }
 /*
 Create a class called UniqueCharacters, Write a program that can return the unique characters from a String
