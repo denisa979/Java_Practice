f20393ee536017699e519d8eed9496881c6a2a53
denisa <dmutapcic79@hotmail.com>
2024-09-24 01:55:34 -0400

    positive/negative count

--- src/branchingStatements_loop/PositiveNegative.java
+++ src/branchingStatements_loop/PositiveNegative.java
@@ -1,6 +1,22 @@
 package branchingStatements_loop;
 
+import java.util.function.DoubleToIntFunction;
+
 public class PositiveNegative {
+    public static void main(String[] args) {
+        int [] input = {10, 20 -1, 0, 3};
+        int positiveCount = 0;
+        int negativeCount = 0;
+
+        for(int num : input){
+            if(num>0){
+                positiveCount++;
+            }else{
+                negativeCount++;
+            }
+        }
+        System.out.println(positiveCount+ " positive " + negativeCount + " negative");
+    }
 }
 /*
 Create a class named PositiveNegative and write a program that asks user to enter number for 5 times, and print how many positive and negative numbers user entered
